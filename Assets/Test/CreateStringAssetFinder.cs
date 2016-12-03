0f5871f83b51e7518ecc2634a467d93c3060f71c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-12-04 04:29:22 +0900

    CreateFinderを実装

---
+++ Assets/Test/CreateStringAssetFinder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+using HK.Framework;
+
+namespace HK.Framework.Test
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class CreateStringAssetFinder : MonoBehaviour
+	{
+		[SerializeField]
+		private string defaultString;
+
+		[SerializeField]
+		private StringAsset stringAsset;
+
+		[SerializeField]
+		private StringAsset.Finder finder;
+
+		void Start()
+		{
+			this.finder = this.stringAsset.CreateFinder(this.defaultString);
+			Debug.Log(this.finder);
+		}
+	}
+}
