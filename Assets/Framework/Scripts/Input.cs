0eaa4f6997657320d9f9e536ce9c00f283d8e1bc
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-12-17 02:44:17 +0900

    Input.cs

---
+++ Assets/Framework/Scripts/Input.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+
+namespace HK.Framework
+{
+	/// <summary>
+	/// 入力制御.
+	/// </summary>
+	public static class Input
+	{
+		
+	}
+}
