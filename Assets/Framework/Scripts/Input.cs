24edea061b6452e40582a02e67726bcfefb3018c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-12-17 02:53:26 +0900

    IsTouch実装

--- Assets/Framework/Scripts/Input.cs
+++ Assets/Framework/Scripts/Input.cs
@@ -9,6 +9,32 @@ namespace HK.Framework
 	/// </summary>
 	public static class Input
 	{
-		
+		/// <summary>
+        /// 画面をタッチしているか返す.
+        /// </summary>
+		/// <remarks>
+		/// コンシューマまたはWebPlayerかUnityの時はマウスの左クリックされているか.
+		/// モバイルの場合はタップされているかで判断しています.
+		/// </remarks>
+        /// <returns>画面をタッチしているか返す.</returns>
+		public static bool IsTouch
+		{
+			get
+			{
+				if(Application.isConsolePlatform || Application.isWebPlayer || Application.isEditor)
+				{
+					return UnityEngine.Input.GetMouseButton(0);
+				}
+				else if(Application.isMobilePlatform)
+				{
+					return UnityEngine.Input.touchCount > 0;
+				}
+				else
+				{
+					Assert.IsTrue(false, "未対応のプラットフォームです. platform = " + Application.platform.ToString());
+					return false;
+				}
+			}
+		}
 	}
 }
