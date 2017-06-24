660a23e4a2312866377c30f4aa249bd64d7e3382
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-06-24 14:31:29 +0900

    :bulb: Assert挟む

--- Assets/HK/Framework/Scripts/System/CsvParser.cs
+++ Assets/HK/Framework/Scripts/System/CsvParser.cs
@@ -12,11 +12,14 @@ namespace HK.Framework
 	{
 		public static List<T> Parse<T>(TextAsset textAsset, System.Func<List<string>, T> parser) where T : class
 		{
+			Assert.IsNotNull(textAsset);
+			Assert.IsNotNull(parser);
 			return textAsset.text.Trim().Replace("\r", "").Split('\n').Where((t, i) => i != 0).Select(t => parser(t.Split(',').ToList())).ToList();
 		}
 
 		public static List<string[]> Split(TextAsset textAsset)
 		{
+			Assert.IsNotNull(textAsset);
 			return textAsset.text.Trim().Replace("\r", "").Split('\n').Where((t, i) => i != 0).Select(t => t.Split(',')).ToList();
 		}
 	}
