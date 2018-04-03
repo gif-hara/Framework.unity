f85012352412ef4ab5e54e9fedd6a11fa1eee638
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-04-04 01:44:53 +0900

    :new: Broker実装

---
+++ Assets/HK/Framework/Scripts/EventSystems/Broker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+namespace HK.Framework.EventSystems
+{
+    /// <summary>
+    /// メッセージを仲介するクラス
+    /// </summary>
+    public static class Broker
+    {
+        /// <summary>
+        /// アプリケーション全体へ通知する<see cref="IMessageBroker"/>
+        /// </summary>
+        public static readonly IMessageBroker Global = MessageBroker.Default;
+        
+        /// <summary>
+        /// <see cref="GameObject"/>に紐づく<see cref="IMessageBroker"/>
+        /// </summary>
+        public static readonly Dictionary<GameObject, IMessageBroker> GameObjects = new Dictionary<GameObject, IMessageBroker>();
+    }
+}
