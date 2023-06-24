9b9740de69af2353bdcbc3bf628cb433eb10d1fb
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-06-24 17:31:41 +0900

    Provider作っていく

--- Packages/HKFramework/Editor/Scripts/FrameworkSettingsProvider.cs
+++ Packages/HKFramework/Editor/Scripts/FrameworkSettingsProvider.cs
@@ -11,7 +11,7 @@ namespace HK.Framework.Editor
             : base(path, scopes, keywords)
         {
         }
-        
+
         [SettingsProvider]
         public static SettingsProvider Create()
         {
@@ -21,6 +21,50 @@ namespace HK.Framework.Editor
                 keywords = new HashSet<string>(new[] { "HKFramework" })
             };
             return provider;
+        }
+
+        public override void OnGUI(string searchContext)
+        {
+            var setupData = Resources.Load<SetupData>("SetupData");
+            if (setupData == null)
+            {
+                if (GUILayout.Button("Create SetupData"))
+                {
+                    // セットアップデータのScriptableObjectを生成してResourcesに配置する
+                    var setupDataInstance = ScriptableObject.CreateInstance<SetupData>();
+                    // フォルダが無い場合は作成する
+                    if (!AssetDatabase.IsValidFolder("Assets/HKFramework"))
+                    {
+                        AssetDatabase.CreateFolder("Assets", "HKFramework");
+                    }
+                    if (!AssetDatabase.IsValidFolder("Assets/HKFramework/Resources"))
+                    {
+                        AssetDatabase.CreateFolder("Assets/HKFramework", "Resources");
+                    }
+                    AssetDatabase.CreateAsset(setupDataInstance, "Assets/HKFramework/Resources/SetupData.asset");
+
+                    // PlayerSettingsのPreloadedAssetsにセットアップデータを追加する
+                    var preloadAssets = new List<Object>(PlayerSettings.GetPreloadedAssets());
+                    preloadAssets.Add(setupDataInstance);
+                    PlayerSettings.SetPreloadedAssets(preloadAssets.ToArray());
+
+                    AssetDatabase.SaveAssets();
+                    AssetDatabase.Refresh();
+                }
+                return;
+            }
+
+            // セットアップデータの中身をGUIで表示する
+            var serializedObject = new SerializedObject(setupData);
+            var iterator = serializedObject.GetIterator();
+            while (iterator.NextVisible(true))
+            {
+                if(iterator.name == "m_Script")
+                {
+                    continue;
+                }
+                EditorGUILayout.PropertyField(iterator, true);
+            }
         }
     }
 }
---
+++ Packages/HKFramework/Runtime/Scripts/SetupData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HK.Framework
+{
+    public class SetupData : ScriptableObject
+    {
+    }
+}
