935d01d732af42fdd752551bcb38e582ed824581
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-07-21 19:44:29 +0900

    必要なアセットを生成するように

--- Packages/HKFramework/Editor/Scripts/FrameworkSettingsProvider.cs
+++ Packages/HKFramework/Editor/Scripts/FrameworkSettingsProvider.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using HK.Framework.BootSystems;
 using HK.Framework.UISystems;
 using UnityEditor;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.UI;
@@ -48,6 +49,7 @@ namespace HK.Framework.Editor
                     AssetDatabase.CreateAsset(setupData, "Assets/HKFramework/Resources/SetupData.asset");
 
                     setupData.SetUIManagerPrefabEditor(CreateDefaultUIManager());
+                    CreateDefaultAnimationData();
                     EditorUtility.SetDirty(setupData);
                     AssetDatabase.SaveAssets();
                     AssetDatabase.Refresh();
@@ -134,6 +136,40 @@ namespace HK.Framework.Editor
             Object.DestroyImmediate(uiManager.gameObject);
 
             return result;
+        }
+
+        private static void CreateDefaultAnimationData()
+        {
+            // BaseとClipAとClipBの3つのAnimationClipを作成する
+            var baseClip = new AnimationClip();
+            AssetDatabase.CreateAsset(baseClip, "Assets/HKFramework/Base.anim");
+            var clipA = new AnimationClip();
+            AssetDatabase.CreateAsset(clipA, "Assets/HKFramework/ClipA.anim");
+            var clipB = new AnimationClip();
+            AssetDatabase.CreateAsset(clipB, "Assets/HKFramework/ClipB.anim");
+            
+            // Runtimeという名前のAnimatorControllerを作成する
+            var controller = new AnimatorController();
+            AssetDatabase.CreateAsset(controller, "Assets/HKFramework/Runtime.controller");
+            // Base LayerとOverride Layer AとOverride Layer Bいう名前のレイヤーを作成する
+            controller.AddLayer("Base Layer");
+            controller.AddLayer("Override Layer A");
+            controller.AddLayer("Override Layer B");
+            
+            // Base LayerにBaseという名前のステートを作成する
+            var baseState = controller.layers[0].stateMachine.AddState("Base");
+            // BaseステートにbaseClipを設定する
+            controller.SetStateEffectiveMotion(baseState, baseClip);
+            
+            // Override Layer AにOverride State Aという名前のステートを作成する
+            var clipAState = controller.layers[1].stateMachine.AddState("Override State A");
+            // Override State AステートにclipAを設定する
+            controller.SetStateEffectiveMotion(clipAState, clipA);
+            
+            // Override Layer BにOverride State Bという名前のステートを作成する
+            var clipBState = controller.layers[2].stateMachine.AddState("Override State B");
+            // Override State BステートにclipBを設定する
+            controller.SetStateEffectiveMotion(clipBState, clipB);
         }
     }
 }
