aa2c9427854ecad25beff6dce23d508b7de2cf22
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-07-21 20:26:56 +0900

    テスト

--- Assets/Test/TestSceneController.cs
+++ Assets/Test/TestSceneController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using HK.Framework.AnimationSystems;
 using UnityEngine;
 
 public class TestSceneController : MonoBehaviour
 {
+    [SerializeField]
+    private AnimationClip testClip;
+    
+    [SerializeField]
+    private AnimationController animationController;
+    
     private async void Start()
     {
         Debug.Log("TestSceneController.Start");
@@ -11,5 +18,12 @@ public class TestSceneController : MonoBehaviour
         Debug.Log("TestSceneController.Start: BootSystem.IsReady 1");
         await HK.Framework.BootSystems.BootSystem.IsReady;
         Debug.Log("TestSceneController.Start: BootSystem.IsReady 2");
+        
+        var animationBlendData = new AnimationBlendData()
+        {
+            animationClip = testClip,
+            blendSeconds = 0.0f,
+        };
+        animationController.Play(animationBlendData);
     }
 }
