8308c1137a29e398a45cc70ad5decc6a49dcea55
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2023-07-27 21:55:24 +0900

    ApplicationQuitObserver追加

---
+++ Packages/HKFramework/Runtime/Scripts/ApplicationQuitObserver.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace HK.Framework
+{
+    /// <summary>
+    /// ApplicationQuitを監視するクラス
+    /// </summary>
+    /// <remarks>
+    /// よくあるシーン再生終了したら後処理でエラーになる問題を解決するために作成
+    /// </remarks>
+    public static class ApplicationQuitObserver
+    {
+        public static bool IsQuit { get; private set; }
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            Application.quitting -= OnApplicationQuit;
+            Application.quitting += OnApplicationQuit;
+            IsQuit = false;
+        }
+        
+        private static void OnApplicationQuit()
+        {
+            IsQuit = true;
+        }
+    }
+}
